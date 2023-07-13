aa46fb5c38bdecf5247de1ed231b6394106871f3
Arthur Sobreira <arsobrei@c1r9p1.42sp.org.br>
2023-07-13 11:38:05 -0300

    Update ft_display_file.c

--- ex27/functions/ft_display_file.c
+++ ex27/functions/ft_display_file.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 18:05:35 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/07/13 11:24:00 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/07/13 11:35:09 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,20 @@ int	ft_display_file(char *file_name)
 	file_desc = open(file_name, O_RDONLY);
 	if (file_desc == -1)
 	{
-		return (1);
+		return (0);
 	}
-	else
+	while (byte_nb > 0)
 	{
-		while (byte_nb > 0)
+		byte_nb = read(file_desc, buff, 1);
+		if (byte_nb == -1)
 		{
-			byte_nb = read(file_desc, buff, 1);
-			if (byte_nb == -1)
-			{
-				return (0);
-			}
-			else if (byte_nb > 0)
-			{
-				write(1, &buff[0], 1);
-			}
+			return (0);
+		}
+		else if (byte_nb > 0)
+		{
+			write(1, &buff[0], 1);
 		}
 	}
-	if (close(file_desc) == -1)
-	{
-		return (1);
-	}
+	close(file_desc);
 	return (0);
 }
